d985227ec1a8ba8d73e3d461f5f66c6e7d31c212
coke05288 <coke05288@gmail.com>
2022-12-27 13:57:58 +0900

    Feat : WorkPost 모델 생성

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import WorkPost
 
-# Register your models here.
+admin.site.register(WorkPost)
--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class WorkPost(models.Model):
+    title = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    #author : 추후 작성
+    
+    url = models.URLField()
+    content = models.TextField()
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
